025c3b38fb94ee97042ed7cefca58487fda0b25c
nicholasnbg <nicholasnbg@gmail.com>
2018-05-14 12:48:07 +0800

    Componentized AddPanel form

---
+++ src/components/AddPanel.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+export default class AddPanel extends Component {
+  
+
+    state = {
+        isIndefinite: false
+    }
+  
+      
+    createTodo = (e) =>{
+        e.preventDefault();
+        console.log('addoing todo item');
+        const newTodo = {
+          heading: this.heading.value,
+          initDate: '03/05/2018', /*MUST MAKE THIS DYNAMIC*/
+          dueDate: this.dueDate.value,
+          indefinite: this.state.isIndefinite,
+          dueTime: this.dueTime.value,
+          location: this.location.value,
+          completed: false
+        }
+        this.props.addTodo(newTodo);
+        this.addTodoForm.reset();
+        !this.dueDate.disabled;
+        !this.dueTime.disabled;    
+    }
+
+    changeIndefinite = () => {
+        console.log('changing indef')
+        if(this.state.isIndefinite){
+            this.setState({
+            isIndefinite: false
+            })
+        } else {
+            this.setState({
+                isIndefinite: true
+            })}        
+    }
+    render() {
+
+    return (
+      <React.Fragment>
+        <div className="addPanel">
+            <form 
+            ref={(input) => this.addTodoForm = input} 
+            onSubmit={(e) => this.createTodo(e)}
+            >
+                <span><label>To Do: </label><input ref={(input) => this.heading = input} type="text" name="heading"/></span>
+                <span><label>Due Date: </label><input disabled={this.state.isIndefinite} type="text" ref={(input) => this.dueDate = input} name="dueDate"/> <br/>or <br/> Indefinite: <input type="checkbox" onClick={this.changeIndefinite} ref={(input) => this.indefinite = input} name="indefinite"/></span>
+                <span><label>Due Time: </label><input disabled={this.state.isIndefinite} type="text" ref={(input) => this.dueTime = input} name="dueTime"/></span>
+                <span><label>Location: </label><input type="text" ref={(input) => this.location = input} name="location" /></span>   
+                <button type="submit">Add +</button>           
+            </form>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
--- src/components/TodoDetail.js
+++ src/components/TodoDetail.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react'
+import AddPanel from './AddPanel'
 
 export default class TodoDetail extends Component {
   state = {
     isIndefinite: false
   }
   
-  createTodo = (e) =>{
-    e.preventDefault();
-    console.log('addoing todo item');
-    const newTodo = {
-      heading: this.heading.value,
-      initDate: '03/05/2018', /*MUST MAKE THIS DYNAMIC*/
-      dueDate: this.dueDate.value,
-      indefinite: this.state.isIndefinite,
-      dueTime: this.dueTime.value,
-      location: this.location.value,
-      completed: false
-    }
-    this.props.addTodo(newTodo);
-    this.addTodoForm.reset();
-    !this.dueDate.disabled;
-    !this.dueTime.disabled;    
-  }
+  
 
   changeIndefinite = () => {
     console.log('changing indef')
@@ -41,18 +26,10 @@ export default class TodoDetail extends Component {
   render() {
     return (
       <div className="details-panel">
-
-      {/* Add panel, maybe componentize this later */}
         {this.props.detailsStatus==="add" && (
-          <div className="addPanel">
-            <form ref={(input) => this.addTodoForm = input} onSubmit={(e) => this.createTodo(e)}>
-              <span><label>To Do: </label><input ref={(input) => this.heading = input} type="text" name="heading"/></span>
-              <span><label>Due Date: </label><input disabled={this.state.isIndefinite} type="text" ref={(input) => this.dueDate = input} name="dueDate"/> <br/>or <br/> Indefinite: <input type="checkbox" onClick={this.changeIndefinite} ref={(input) => this.indefinite = input} name="indefinite"/></span>
-              <span><label>Due Time: </label><input disabled={this.state.isIndefinite} type="text" ref={(input) => this.dueTime = input} name="dueTime"/></span>
-              <span><label>Location: </label><input type="text" ref={(input) => this.location = input} name="location" /></span>   
-              <button type="submit">Add +</button>           
-            </form>
-          </div>
+          <AddPanel
+            addTodo={this.props.addTodo}
+          />
         )}
 
 
